16269253bf387fea46d0ce6bd2303bb3386efe0d
Faris Alhammad <faris@MacBook-Air-Faris.local>
2025-03-06 20:33:03 +0300

    STATICFILES_DIRS path fixed to be dynamic

--- website/website/settings.py
+++ website/website/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [( os.path.join(BASE_DIR, "apps\static"))]
+# STATICFILES_DIRS = [( os.path.join(BASE_DIR, "apps\static"))]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "apps", "static")] # use this for paths 
 print('STATICFILES_DIRS--------------------------------------------------: ',STATICFILES_DIRS)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
