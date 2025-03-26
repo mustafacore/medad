bd1599f55a4db51475bbb566f866908d94fd0532
Faris Alhammad <faris@MacBook-Air-Faris.local>
2025-03-26 04:30:31 +0300

    add download icon

--- website/website/urls.py
+++ website/website/urls.py
@@ -20,7 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include("apps.usermodule.urls"))
 ]
 
