cb4a4f3d5c57f8b7c6e6cca7a58ed9c0b7e6943c
MUSTAFA <msallat5@gmail.com>
2025-05-25 19:32:29 +0300

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -15,16 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
+from django.views.static import serve
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include("apps.usermodule.urls"))
 ]
 
-if settings.DEBUG is False:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
